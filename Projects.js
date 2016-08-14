a32e16b25954946b063f2ce6f4d17da572dbaf0d
CamrynD <camryndavis98@gmail.com>
2016-08-14 14:10:36 -0400

    Create Projects.js

---
+++ Projects.js
@@ -0,0 +1,7 @@
+var project_Table = [
+  //Project name, Owner/Developer, URL to project
+  ["<u><sup>08,13,16</sup></u>&nbsp;Tigertech - Batch","Camryn Davis",""],
+]
+function getProjects() {
+    return project_Table
+}
