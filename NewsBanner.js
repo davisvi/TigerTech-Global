91ec1e97da04f5fcd89ddeca069892c968360a0f
CamrynD <CamrynD@users.noreply.github.com>
2016-08-19 09:12:49 -0400

    Update NewsBanner.js

--- NewsBanner.js
+++ NewsBanner.js
@@ -10,6 +10,7 @@ var news_Table = [
     "<u><sup>08,01,16</sup></u>&nbsp;Now linked to remotely hosted library",
     "<u><sup>08,13,16</sup></u>&nbsp;Final Revisions, checking all source code.",
     "<u><sup>08,14,16</sup></u>&nbsp;Version 3! new linking script!",
+    "<u><sup>08,19,16</sup></u>&nbsp;Working on a demo section",
 ]
 var link_Table = [
         "https://lebanon.instructure.com/login/ldap",
